ccd92a2d9ac88f7e7cb86f7121b751d6ee7b5782
swathi <swathi2920@gmail.com>
2020-09-24 21:01:51 +0530

    done

---
+++ stud.java
@@ -0,0 +1,17 @@
+
+public class stud
+{
+public static void main(String[] args) 
+{
+if(args.length == 0 )
+ {
+System.err.println("No input entered...");
+System.exit(1); 
+}
+
+System.out.println("prog closed...");
+System.out.println(args[0]);
+System.out.println(args[1]);
+}
+}
+
